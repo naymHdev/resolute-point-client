1cf1ebed23a658856230f660ef4506e00913ac1c
MacBook Pro <naym100m@gmail.com>
2024-03-20 19:29:44 +0600

    install || axios and tanstack || and intial setup compleate

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import MainRouter from "./Router/MainRouter";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={MainRouter} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={MainRouter} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
