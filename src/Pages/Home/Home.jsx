c5107fd9e00adb2b9d4e6461a0ed556cdd517101
MacBook Pro <naym100m@gmail.com>
2024-03-21 11:39:41 +0600

    updated a home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,11 @@
-
+import banner from "../../assets/home page.jpg";
 
 const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <img className=" w-full h-[100vh]" src={banner} alt="" />
+    </div>
+  );
 };
 
 export default Home;
