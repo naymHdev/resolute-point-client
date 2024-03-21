11eb2ee3873c55454393fe8f48fd70af72def6ee
MacBook Pro <naym100m@gmail.com>
2024-03-21 12:05:42 +0600

    Private route spiner change

--- src/Router/PrivateRoute.jsx
+++ src/Router/PrivateRoute.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../Hooks/useAuth";
+import { ImSpinner10 } from "react-icons/im";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useAuth();
@@ -8,7 +9,13 @@ const PrivateRoute = ({ children }) => {
   const location = useLocation();
 
   if (loading) {
-    return <progress className="progress w-full"></progress>;
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex items-center">
+          <ImSpinner10 className="text-9xl" />
+        </div>
+      </div>
+    );
   }
 
   if (user?.email) {
